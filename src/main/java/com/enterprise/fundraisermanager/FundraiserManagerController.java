514f6f3d5f9d149c5dbcf6b59375ee96022a6810
Jacob Gormly <gormlyja@mail.uc.edu>
2022-02-07 12:27:40 -0500

    Added Gets, Post, and Delete in controller

--- src/main/java/com/enterprise/fundraisermanager/FundraiserManagerController.java
+++ src/main/java/com/enterprise/fundraisermanager/FundraiserManagerController.java
@@ -1,18 +1,42 @@
 package com.enterprise.fundraisermanager;
 
+import com.enterprise.fundraisermanager.dto.Fundraiser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class FundraiserManagerController {
 
     /**
      * Handle the root (/) endpoint and return a start page.
-     * @return
+     * @return start page
      */
     @RequestMapping("/")
     public String index(){
         return "start";
     }
+
+    @GetMapping("/fundraiser")
+    public ResponseEntity fetchAllFundraisers() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/fundraiser/{id}/")
+    public ResponseEntity fetchFundraiserByID(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping(value="/fundraiser", consumes="application/json", produces = "application/json")
+    public Fundraiser createFundraiser(@RequestBody Fundraiser fundraiser) {
+        return fundraiser;
+    }
+
+    @DeleteMapping("/fundraiser/{id}/")
+    public ResponseEntity deleteFundraiser(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
 
 }
