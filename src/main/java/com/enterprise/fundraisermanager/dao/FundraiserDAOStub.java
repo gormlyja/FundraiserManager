9a100baf2000ab4526ac8eea7d993866511dacb9
Jacob Gormly <gormlyja@mail.uc.edu>
2022-02-09 19:53:17 -0500

    Rename

--- src/main/java/com/enterprise/fundraisermanager/dao/FundraiserDAOStub.java
+++ src/main/java/com/enterprise/fundraisermanager/dao/FundraiserDAOStub.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 @Repository
-public class SpecimenDAOStub implements IFundraiserDAO{
+public class FundraiserDAOStub implements IFundraiserDAO{
 
     Map<Integer, Fundraiser> allFundraisers = new HashMap<>();
 
