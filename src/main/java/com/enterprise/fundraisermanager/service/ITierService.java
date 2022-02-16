734595c43bd9b725f8f654d59d9a5147d884bced
Vedant Dharmesh Amin <54548039+aminvd@users.noreply.github.com>
2022-02-16 17:48:28 -0500

    Update I Tier Service

--- src/main/java/com/enterprise/fundraisermanager/service/ITierService.java
+++ src/main/java/com/enterprise/fundraisermanager/service/ITierService.java
@@ -1,4 +1,17 @@
 package com.enterprise.fundraisermanager.service;
 
+import com.enterprise.fundraisermanager.dto.Fundraiser;
+import com.enterprise.fundraisermanager.dto.Tier;
+
+import java.util.List;
+
 public interface ITierService {
+
+    Tier fetchById(int tierId, Fundraiser fundraiser);
+
+    void delete(int tierId) throws Exception;
+
+    Tier save(Tier tier) throws Exception;
+
+    List<Tier> fetchAll();
 }
