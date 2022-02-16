2c59e91ac49f5643a794a46c67b821303d458364
Vedant Dharmesh Amin <54548039+aminvd@users.noreply.github.com>
2022-02-16 15:43:47 -0500

    Updated Tier.java
    removed int level

--- src/main/java/com/enterprise/fundraisermanager/dto/Tier.java
+++ src/main/java/com/enterprise/fundraisermanager/dto/Tier.java
@@ -5,7 +5,6 @@ import lombok.Data;
 public @Data
 class Tier {
     private Integer tierId;
-    private Integer level;
     private String name;
     private String description;
     private Double requiredAmount;
