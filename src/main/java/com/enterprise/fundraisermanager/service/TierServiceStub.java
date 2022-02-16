982a40c1a1672c87fbb6a5249bd88b403a0d3443
Vedant Dharmesh Amin <54548039+aminvd@users.noreply.github.com>
2022-02-16 17:48:41 -0500

    Update Tier Service Stub

--- src/main/java/com/enterprise/fundraisermanager/service/TierServiceStub.java
+++ src/main/java/com/enterprise/fundraisermanager/service/TierServiceStub.java
@@ -1,4 +1,61 @@
 package com.enterprise.fundraisermanager.service;
 
-public class TierServiceStub {
+import com.enterprise.fundraisermanager.dao.ITierDAO;
+import com.enterprise.fundraisermanager.dto.Fundraiser;
+import com.enterprise.fundraisermanager.dto.Tier;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TierServiceStub implements ITierService {
+    private ITierDAO tierDAO;
+
+    public TierServiceStub() {
+}
+
+    public TierServiceStub(ITierDAO tierDAO) {
+        this.tierDAO = tierDAO;
+    }
+
+    @Override
+    public Tier fetchById(int tierId, Fundraiser fundraiser) {
+        Tier tier = new Tier();
+        tier.setDescription("");
+
+        Tier newTier = new Tier();
+        newTier.setName("Tier 1");
+        newTier.setRequiredAmount(5.00);
+
+        ArrayList<Tier> tierList = new ArrayList<>();
+        tierList.add(0, newTier);
+        fundraiser.setTierList(tierList);
+
+        return tierList.get(0);
+
+
+
+    }
+
+    @Override
+    public void delete(int tierId) throws Exception {
+        tierDAO.delete(tierId);
+    }
+
+    @Override
+    public Tier save(Tier tier) throws Exception{
+        return tierDAO.save(tier);
+    }
+
+    @Override
+    public List<Tier> fetchAll() {
+        return tierDAO.fetchAll();
+    }
+
+    /**
+     * Any methods not included in lecture. TODO Remove this later in project
+     *
+     * @param fundraiser
+     */
 }
