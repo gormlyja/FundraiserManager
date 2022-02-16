41d04a5b377440444e9433e37ba2aeeba23dfc15
Vedant Dharmesh Amin <54548039+aminvd@users.noreply.github.com>
2022-02-16 17:47:04 -0500

    Created Tier DAO Stub

---
+++ src/main/java/com/enterprise/fundraisermanager/dao/TierDAOStub.java
@@ -0,0 +1,36 @@
+package com.enterprise.fundraisermanager.dao;
+
+import com.enterprise.fundraisermanager.dto.Fundraiser;
+import com.enterprise.fundraisermanager.dto.Tier;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class TierDAOStub implements ITierDAO{
+    Map<Integer, Tier> allTiers = new HashMap<>();
+
+    @Override
+    public Tier save(Tier tier) throws Exception {
+        allTiers.put(tier.getTierId(), tier);
+        return tier;
+    }
+
+    @Override
+    public List<Tier> fetchAll() {
+        List<Tier> returnTiers = new ArrayList<>(allTiers.values());
+        return returnTiers;
+    }
+
+    @Override
+    public Tier fetch(int tierId) {
+        return allTiers.get(tierId);
+    }
+
+    @Override
+    public void delete(int tierId) {
+        allTiers.remove(tierId);
+    }
+}
