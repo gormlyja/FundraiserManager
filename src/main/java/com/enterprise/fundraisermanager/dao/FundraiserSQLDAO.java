87832249741ee30d2c4465338185041b60c7e426
Nathan Coffee <coffeens@mail.uc.edu>
2022-03-27 18:55:27 -0400

    Created DB and added DB connection to application.properties Created initial FundraiserSQLDAO and repo

---
+++ src/main/java/com/enterprise/fundraisermanager/dao/FundraiserSQLDAO.java
@@ -0,0 +1,28 @@
+package com.enterprise.fundraisermanager.dao;
+
+import com.enterprise.fundraisermanager.dto.Fundraiser;
+
+import java.util.List;
+
+public class FundraiserSQLDAO implements IFundraiserDAO {
+
+    @Override
+    public Fundraiser save(Fundraiser fundraiser) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Fundraiser> fetchAll() {
+        return null;
+    }
+
+    @Override
+    public Fundraiser fetch(int id) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
