078a4b1efe27491cb8ea0f35f54a1040787a228f
Rahul Shakya <shakyarl@mail.uc.edu>
2022-04-17 16:24:26 -0400

    Added annotations for sql table creation and connection, for many-to-one relation with tiers

--- src/main/java/com/enterprise/fundraisermanager/dto/User.java
+++ src/main/java/com/enterprise/fundraisermanager/dto/User.java
@@ -2,15 +2,22 @@ package com.enterprise.fundraisermanager.dto;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.HashMap;
 
+@Entity
+@Table (name = "Users")
 public @Data
 class User {
+    @Id
+    @GeneratedValue
     private Integer userId;
     private String username;
     private String password;
     private String firstName;
     private String lastName;
     private String emailAddress;
-    private HashMap<Integer, Fundraiser> userFundraiserMap;
 }
