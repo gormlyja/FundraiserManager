a1f447d455ed62847c0ebb66fb23bad4043d355b
Vedant Dharmesh Amin <54548039+aminvd@users.noreply.github.com>
2022-04-18 19:19:57 -0400

    Update IEventRetrofitDAO.java

--- src/main/java/com/enterprise/fundraisermanager/dao/IEventRetrofitDAO.java
+++ src/main/java/com/enterprise/fundraisermanager/dao/IEventRetrofitDAO.java
@@ -1,4 +1,13 @@
 package com.enterprise.fundraisermanager.dao;
+import com.enterprise.fundraisermanager.dto.Fundraiser;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
-public class IEventRetrofitDAO {
+import java.util.List;
+
+public interface IEventRetrofitDAO {
+
+    @GET("/fundraisers.json")
+    Call<List<Fundraiser>> getFundraisers(@Query("Combined_Name") String combinedName);
 }
