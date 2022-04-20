59d3a809c027354f65faa4fec9e83377ccfa0d17
Vedant Dharmesh Amin <54548039+aminvd@users.noreply.github.com>
2022-04-20 00:35:53 -0400

    Revert "Create FundraiserRepository.java"
    This reverts commit 9817f2b5af1b65191a735fc2cf1a6f33c7a0b4d6.

--- src/main/java/com/enterprise/fundraisermanager/dao/FundraiserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.enterprise.fundraisermanager.dao;
-
-import com.enterprise.fundraisermanager.dto.Fundraiser;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FundraiserRepository extends CrudRepository<Fundraiser, Integer> { 
-}
